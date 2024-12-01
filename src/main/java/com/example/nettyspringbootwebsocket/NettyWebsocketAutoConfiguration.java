92cd90838da614704668e700ae8b418e3c35618c
k <824135348@qq.com>
2024-12-02 00:41:25 +0800

    基于Spring的SPI机制注入配置类

---
+++ src/main/java/com/example/nettyspringbootwebsocket/NettyWebsocketAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.nettyspringbootwebsocket;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author : KangJunJie
+ * @Date : 2024/12/2
+ */
+@Configuration
+public class NettyWebsocketAutoConfiguration {
+
+//    @Bean
+//    public WebSocketAnnotationPostProcessor webSocketAnnotationPostProcessor() {
+//        return new WebSocketAnnotationPostProcessor();
+//    }
+
+//    @Bean
+//    public WebsocketProperties websocketProperties() {
+//        return new WebsocketProperties();
+//    }
+}
