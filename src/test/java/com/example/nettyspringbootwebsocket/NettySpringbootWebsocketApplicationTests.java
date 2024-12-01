eb11fa20245dda67c1288141c88391e8ab206380
k <824135348@qq.com>
2024-12-01 21:37:12 +0800

    建立Spring

---
+++ src/test/java/com/example/nettyspringbootwebsocket/NettySpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nettyspringbootwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettySpringbootWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
